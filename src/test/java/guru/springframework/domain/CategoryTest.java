3bb28634c8baaa7487078dc03bdbfad2122d9fe7
Collin Shriner <collinshriner9@gmail.com>
2023-06-21 17:13:46 -0400

    added some basic unit tests for category class

--- src/test/java/guru/springframework/domain/CategoryTest.java
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -3,22 +3,19 @@ package guru.springframework.domain;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-/**
- * Created by jt on 6/17/17.
- */
 public class CategoryTest {
 
     Category category;
 
     @Before
-    public void setUp(){
-        category = new Category();
+    public void setUp()
+    {
+        category=new Category();
     }
-
     @Test
-    public void getId() throws Exception {
+    public void getId() {
         Long idValue = 4L;
 
         category.setId(idValue);
@@ -27,11 +24,14 @@ public class CategoryTest {
     }
 
     @Test
-    public void getDescription() throws Exception {
+    public void getDescription() {
+
+        category.setDescription("Description");
+
+        assertEquals("Description", category.getDescription());
     }
 
     @Test
-    public void getRecipes() throws Exception {
+    public void getRecipes() {
     }
-
 }
