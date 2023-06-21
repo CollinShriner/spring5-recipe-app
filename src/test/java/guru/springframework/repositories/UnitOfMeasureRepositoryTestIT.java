1fd1d730a5caaef6677059464be1bbdb8daee820
Collin Shriner <collinshriner9@gmail.com>
2023-06-21 18:03:34 -0400

    added a basic integration test for unit of measure repository

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,12 @@
+package guru.springframework.repositories;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Test
+    public void findByDescription() {
+    }
+}
