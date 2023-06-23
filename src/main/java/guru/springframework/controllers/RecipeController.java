ab821a6322fe2fe2baa147a93ebc74c73a53cc27
Collin Shriner <collinshriner9@gmail.com>
2023-06-23 11:52:40 -0400

    added update and delete recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -16,10 +16,42 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
-    public String showById(Model model, @PathVariable Long id) throws NotFoundException {
-        model.addAttribute("recipe", recipeService.findById(id));
+    @GetMapping
+    @RequestMapping("/recipe/{id}/show")
+    public String showById(Model model, @PathVariable String id) throws NotFoundException {
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model)
+    {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping(name = "recipe", method = RequestMethod.POST)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command)
+    {
+        RecipeCommand saveCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/"+saveCommand.getId()+"/show/";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) throws NotFoundException {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/recipeform";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id, Model model)
+    {
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
